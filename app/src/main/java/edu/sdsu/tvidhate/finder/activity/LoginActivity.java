372b22c861668f080bba111909ef2e58e6714a17
tanvi <10vidhate@gmail.com>
2018-06-18 19:22:24 -0700

    Initial commit

---
+++ app/src/main/java/edu/sdsu/tvidhate/finder/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package edu.sdsu.tvidhate.finder.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.sdsu.tvidhate.finder.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
